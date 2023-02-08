e603c8df2543e0fa94b852a07ec2e10c79a4d788
ivan popov <ivan.alekseevich.popov@gmail.com>
2023-02-08 23:07:27 +0400

    baseline

--- app/src/main/java/com/jetpack/carpartsfinder/view/component/SearchBlockView.kt
+++ app/src/main/java/com/jetpack/carpartsfinder/view/component/SearchBlockView.kt
@@ -1,4 +1,4 @@
-package com.jetpack.carpartsfinder.view
+package com.jetpack.carpartsfinder.view.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
